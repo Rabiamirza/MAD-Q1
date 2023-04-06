bcdd878737057cc7e7b6a1f10ce88a163d56f47d
Rabiamirza <96281296+Rabiamirza@users.noreply.github.com>
2023-04-06 23:37:05 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Exercise1 from './Exercise1';
+import Exercise2 from './Exercise2';
+import Exercise3 from './Exercise3';
+import Exercise4 from './Exercise4';
+import AboutUs from './AboutUs';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    
+   
+      
+
+
+
+      <Exercise1 />
+      
+
+
+      
+     
+  );
+}
+
+export default App;
